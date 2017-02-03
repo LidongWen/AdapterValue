33e27b350269f0e08f7ea1cb8f136d7361062f8b
wenld <wenld2014@163.om>
2017-02-03 13:58:47 +0800

    Initial commit

---
+++ app/src/main/java/com/wenld/adaptervalue/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wenld.adaptervalue;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/2/3.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_main);
+    }
+}
